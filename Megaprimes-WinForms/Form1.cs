c133a76da0310217da9b48dff3e4cfcb7f145d1a
floatPond <32678960+floatPond@users.noreply.github.com>
2022-12-12 02:32:11 +0000

    SC - Added logic for checking if number is prime and checking all mega primes

--- Megaprimes-WinForms/Form1.cs
+++ Megaprimes-WinForms/Form1.cs
@@ -1,3 +1,6 @@
+using Megaprimes_Console;
+using System.Threading;
+
 namespace Megaprimes_WinForms
 {
     public partial class Form1 : Form
@@ -5,6 +8,26 @@ namespace Megaprimes_WinForms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void bCheckIfPrime_Click(object sender, EventArgs e)
+        {
+            lResult.Text = "";
+            if (PrimeNumberController.IsPrime((uint)nudInput.Value))
+            {
+                lResult.Text = "true";
+            }
+            else
+            {
+                lResult.Text = "false";
+            }
+            
+        }
+
+        private void bMegaPrimes_Click(object sender, EventArgs e)
+        {
+            lResult.Text = "";
+            lResult.Text = (PrimeNumberController.MegaPrimeFinderThreaded(1, (uint)nudInput.Value)).TotalSeconds.ToString();
         }
     }
 }
